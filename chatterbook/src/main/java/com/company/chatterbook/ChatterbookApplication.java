ff091a9819a0952d8cd601f3416eb6c21980a8b4
idjones5 <97227301+idjones5@users.noreply.github.com>
2022-08-29 12:35:42 -0700

    pom

---
+++ chatterbook/src/main/java/com/company/chatterbook/ChatterbookApplication.java
@@ -0,0 +1,18 @@
+package com.company.chatterbook;
+
+import com.company.chatterbook.controllers.ChatterPostController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
+@SpringBootApplication
+public class ChatterbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatterbookApplication.class, args);
+
+		// main inside here
+
+		// --> main end
+	}
+}
